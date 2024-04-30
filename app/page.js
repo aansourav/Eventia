fc774529b0755813967aab64ee7fe2533da724ab
SoUrAv <aansourav@gmail.com>
2024-04-30 16:52:31 +0600

    Image blur effect on loading implemented

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import EventList from "@/components/landing/EventList";
 import Header from "@/components/landing/Header";
+import Loading from "@/components/Loading";
+import { Suspense } from "react";
 
 export default function Home({ searchParams: { search } }) {
     return (
         <section className="container">
             <Header />
-            <EventList search={search} />
+            <Suspense key={search} fallback={<Loading />}>
+                <EventList search={search} />
+            </Suspense>
         </section>
     );
 }
